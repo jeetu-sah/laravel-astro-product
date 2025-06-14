8a77d8c3fc385317041ecdb7f76ba65a326e9ffa
Jitendra sahu <jitendrasahu17996@gmail.com>
2025-06-14 22:55:01 +0530

    manage the database

--- database/migrations/2025_05_15_201325_create_orders_table.php
+++ database/migrations/2025_05_15_201325_create_orders_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
 
             $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('astro_products')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
 
 
 
